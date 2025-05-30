a0d39d79f856805c836ef1558e18de5c62447d0a
sonilmahato <sunilmahatoneymar@gmail.com>
2025-05-30 02:12:57 +0100

    Add files via upload

---
+++ src/components/CarData.jsx
@@ -0,0 +1,93 @@
+import AudiA5 from "../images/cars-big/audi-box.png";
+import VWArteon from "../images/cars-big/arteon-box.jpg";
+import ToyotaCorolla from "../images/cars-big/corolla-box.png";
+import Bmw5 from "../images/cars-big/bmw-box.png";
+import KiaSportage from "../images/cars-big/sportage-box.png";
+import Mini from "../images/cars-big/mini-box.png";
+
+export const CAR_DATA = [
+  [
+    {
+      name: "Audi A5",
+      price: "0.037",
+      img: AudiA5,
+      model: "A5",
+      mark: "Audi",
+      year: "2022",
+      doors: "4/5",
+      air: "Yes",
+      transmission: "Automatic",
+      fuel: "Gasoline",
+    },
+  ],
+  [
+    {
+      name: "VW Arteon",
+      price: "0.031",
+      img: VWArteon,
+      model: "Arteon",
+      mark: "VW",
+      year: "2020",
+      doors: "4/5",
+      air: "Yes",
+      transmission: "Automatic",
+      fuel: "Gasoline",
+    },
+  ],
+  [
+    {
+      name: "Toyota Corolla",
+      price: "0.23",
+      img: ToyotaCorolla,
+      model: "Corolla",
+      mark: "Toyota",
+      year: "2023",
+      doors: "4/5",
+      air: "Yes",
+      transmission: "Automatic",
+      fuel: "Hybrid",
+    },
+  ],
+  [
+    {
+      name: "BMW 530",
+      price: "0.034",
+      img: Bmw5,
+      model: "530",
+      mark: "BMW",
+      year: "2022",
+      doors: "4/5",
+      air: "Yes",
+      transmission: "Manual",
+      fuel: "Diesel",
+    },
+  ],
+  [
+    {
+      name: "Kia Sportage",
+      price: "0.025",
+      img: KiaSportage,
+      model: "Sportage",
+      mark: "Kia",
+      year: "2023",
+      doors: "4/5",
+      air: "Yes",
+      transmission: "Automatic",
+      fuel: "Gasoline",
+    },
+  ],
+  [
+    {
+      name: "Mini Cooper",
+      price: "0.02",
+      img: Mini,
+      model: "Cooper",
+      mark: "Mini",
+      year: "2020",
+      doors: "2/3",
+      air: "Yes",
+      transmission: "Manual",
+      fuel: "Gasoline",
+    },
+  ],
+];
